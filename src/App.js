63b88dfb43edb74d6cabf6297e9ec6c4a83f496b
zhongdeming428 <570160841@qq.com>
2017-12-15 14:40:24 +0800

    add the header and the Links

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Links from './Links/Links'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-header">
+          <h1 className="App-title">React Router V4.0 Example</h1>
+        </div>
+        <Links/>
       </div>
     );
   }
---
+++ src/Links/Links.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Links.css'
+
+class Links extends React.Component {
+    render(){
+        return <div className="Links">
+
+        </div>
+    }
+}
+
+export default Links;
